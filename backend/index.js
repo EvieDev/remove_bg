6026b2566e371d6d89734d360ab547ba6e6ee83d
Yocheved Rothstein <yochevedrot@gmail.com>
2024-10-10 17:02:37 +0300

    fix api bug

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,33 @@ app.use(fileUpload());
 
 app.use(cors());
 
+app.post("/get_img", async function (req, res) {
+  //   console.log(req.files);
+
+  let fileName = req.files.file.name;
+  filePath = __dirname + "/uploadImage/" + fileName;
+
+  req.files.file.mv(filePath, async function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("uploaded");
+
+      console.log(filePath);
+      const inputPath = filePath;
+      const fileBlob = await fs.openAsBlob(inputPath);
+      console.log(fileBlob);
+      const rbgResultData = await removeBg(fileBlob);
+      fs.writeFileSync(
+        __dirname + "/noBgImage/" + fileName,
+        Buffer.from(rbgResultData)
+      );
+    }
+  });
+
+  res.send("200");
+});
+
 async function removeBg(blob) {
   const formData = new FormData();
   formData.append("size", "auto");
@@ -26,33 +53,6 @@ async function removeBg(blob) {
     throw new Error(`${response.status}: ${response.statusText}`);
   }
 }
-
-app.post("/get_img", async function (req, res) {
-  //   console.log(req.files);
-
-  let fileName = req.files.file.name;
-  filePath = __dirname + "/uploadImage/" + fileName;
-
-  req.files.file.mv(filePath, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("uploaded");
-    }
-  });
-
-  console.log(filePath);
-  const inputPath = filePath;
-  const fileBlob = await fs.openAsBlob(inputPath);
-  console.log(fileBlob);
-  const rbgResultData = await removeBg(fileBlob);
-  fs.writeFileSync(
-    __dirname + "/noBgImage/" + fileName,
-    Buffer.from(rbgResultData)
-  );
-
-  res.send("200");
-});
 
 console.log("server start at port 3001...");
 
